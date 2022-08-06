7c79977b54124491d6b4e0b1543dc52bbe1b5fad
Daniel Hyres <danhyres@gmail.com>
2022-08-06 11:45:19 -0400

    Updates

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const connectDB = require('./config/db')
 
 //Load config
 dotenv.config({ path: './config/config.env' })
 
+connectDB()
+
 const app = express()
+
+// Logging
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('.hbs', engine({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
 
 const PORT = process.env.PORT || 3000
 
