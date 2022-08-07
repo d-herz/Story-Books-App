514de1458de40eca0342c50ab01b73963a75dbc1
Daniel Hyres <danhyres@gmail.com>
2022-08-07 12:38:42 -0400

    progress

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const { engine } = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
+
 
 //Load config
 dotenv.config({ path: './config/config.env' })
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -21,11 +29,26 @@ if(process.env.NODE_ENV === 'development'){
 app.engine('.hbs', engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
+// Sessions (this needs to be above passport middleware)
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI, }),
+})
+)
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000
 
